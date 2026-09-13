5964e56a00447df668094f1305ab8e182cd44363
Tetiana Petrukha <tan9onyschenko@gmail.com>
2026-09-13 20:08:01 +0300

    add tags to tests

--- tests/emptyCart.spec.ts
+++ tests/emptyCart.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from "@playwright/test";
 
-test("CC-003 empty cart", async ({ page }) => {
-  await page.goto("");
-  await page.getByRole("link", { name: "Cart page" }).click();
-  await expect(page.getByText("No coffee, go add some.")).toBeVisible();
-});
+test(
+  "CC-003 empty cart",
+  { tag: ["@cart", "@smoke", "@regression"] },
+  async ({ page }) => {
+    await page.goto("");
+    await page.getByRole("link", { name: "Cart page" }).click();
+    await expect(page.getByText("No coffee, go add some.")).toBeVisible();
+  },
+);
--- tests/successfullPayment.spec.ts
+++ tests/successfullPayment.spec.ts
@@ -1,12 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test("CC-005 successfull payment", async ({ page }) => {
-  await page.goto("");
-  await page.locator('[data-test="Cappuccino"]').click();
-  await page.locator('[data-test="checkout"]').click();
-  await page.getByRole("textbox", { name: "Name" }).fill("tatka");
-  await page.getByRole("textbox", { name: "Email" }).fill("tatka@gmail.com");
-  await expect(
-    page.getByRole("button", { name: "Thanks for your purchase." }),
-  ).toBeVisible();
-});
+test(
+  "CC-005 successfull order",
+  { tag: ["@order", "@smoke", "@regression"] },
+  async ({ page }) => {
+    await page.goto("");
+    await page.locator('[data-test="Cappuccino"]').click();
+    await page.locator('[data-test="checkout"]').click();
+    await page.getByRole("textbox", { name: "Name" }).fill("tatka");
+    await page.getByRole("textbox", { name: "Email" }).fill("tatka@gmail.com");
+    await expect(
+      page.getByRole("button", { name: "Thanks for your purchase." }),
+    ).toBeVisible();
+  },
+);
